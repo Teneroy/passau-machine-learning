558dc403f49097ebfa84027e37b65d6c1fd502cd
Aleksei <alexchernishof@gmail.com>
2022-06-09 01:30:22 +0200

    Added linear tb, fixed regression

--- alekseiml/regressions.py
+++ alekseiml/regressions.py
@@ -1,8 +1,8 @@
-from base import Regression, BaseModel
+from base import  Regression, BaseModel
 import numpy as np
 
 
-class RandomNormalEstimator(Regression, BaseModel):
+class RandomNormalEstimator(BaseModel):
     def __init__(self):
         self._mean = 0
         self._std = 1
@@ -16,7 +16,7 @@ class RandomNormalEstimator(Regression, BaseModel):
         return np.random.normal(self._mean, self._std, (features.shape[0],))
 
 
-class RandomUniformEstimator(Regression, BaseModel):
+class RandomUniformEstimator(BaseModel):
     def __init__(self):
         self._tmin = 0
         self._tmax = 1
@@ -30,7 +30,7 @@ class RandomUniformEstimator(Regression, BaseModel):
         return np.random.uniform(self._tmin, self._tmax, (features.shape[0],))
 
 
-class LinearRegressionLMS(BaseModel, Regression):
+class LinearRegressionLMS(BaseModel):
     def __init__(self) -> None:
         self.w = None
 
@@ -38,17 +38,17 @@ class LinearRegressionLMS(BaseModel, Regression):
         return np.hstack((features, np.ones((len(features), 1))))
 
     def learn(self, features: np.ndarray, targets: np.ndarray) -> None:
-        self._check_learn_shapes(features, targets)
+        # self._check_learn_shapes(features, targets)
         features = self._add_constant(features)
         self.w = np.linalg.inv(features.T @ features) @ features.T @ targets  # there is also np.linalg.pinv
 
     def infer(self, features: np.ndarray) -> np.ndarray:
-        self._check_infer_shapes(features)
+        # self._check_infer_shapes(features)
         features = self._add_constant(features)
         return features @ self.w
 
 
-class LinearRegressorGD(BaseModel, Regression):
+class LinearRegressorGD(BaseModel):
     def __init__(self) -> None:
         self.w = None
 
@@ -63,19 +63,80 @@ class LinearRegressorGD(BaseModel, Regression):
             n_epochs: int = 500,
             random_state: int = 42,
     ) -> None:
-        self._check_learn_shapes(features, targets)
+        # self._check_learn_shapes(features, targets)
         features = self._add_constant(features)
 
         rng = np.random.default_rng(random_state)
         self.w = rng.standard_normal(size=(features.shape[1],))
 
-        for _ in range(n_epochs):
+        for epoch in range(n_epochs):
+            # print(epoch)
+            gd = self._gradient(features, targets)
             self.w = self.w - learning_rate * self._gradient(features, targets)
 
     def _gradient(self, features: np.ndarray, targets: np.ndarray):
-        return features.T @ features @ self.w - features.T @ targets
+        return features.T @ features @ self.w - (features.T @ targets)
 
     def infer(self, features: np.ndarray) -> np.ndarray:
-        self._check_infer_shapes(features)
+        # self._check_infer_shapes(features)
         features = self._add_constant(features)
         return features @ self.w
+
+
+class LinearRegressorGD2(Regression):
+    def __init__(self) -> None:
+        self.w = None
+
+    def _add_constant(self, X: np.ndarray) -> np.ndarray:
+        """Add a constant column to a matrix.
+
+        Args:
+            X (np.ndarray): Original data matrix
+
+        Returns:
+            np.ndarray: Original data matrix with concatenated column of all ones.
+        """
+        return np.hstack((X, np.ones((len(X), 1))))
+
+    def learn(
+            self,
+            X: np.ndarray,
+            y: np.ndarray,
+            learning_rate: float = 1e-3,
+            n_epochs: int = 500,
+            random_state: int = 42,
+    ) -> None:
+        """Fit the parameters of the model to the data with gradient descent.
+
+        Args:
+            X (np.ndarray): features
+            y (np.ndarray): targets
+            learning_rate (float): step size of gradient descent
+            n_epochs (int): number of parameter updates
+            random_state (int): seed for reproducibility
+        """
+        # modify the features such that a bias can be learned easily
+        X = self._add_constant(X)
+
+        # initialize randomly
+        rng = np.random.default_rng(random_state)
+        self.w = rng.standard_normal(size=(X.shape[1],))
+
+        # gradient descent
+        for _ in range(n_epochs):
+            self.w = self.w - learning_rate * self._gradient(X, y)
+
+    def _gradient(self, X: np.ndarray, y: np.ndarray):
+        return X.T @ X @ self.w - X.T @ y
+
+    def infer(self, X: np.ndarray) -> np.ndarray:
+        """Use parameters to predict values
+
+        Args:
+            X (np.ndarray): features
+
+        Returns:
+            np.ndarray: predicted targets
+        """
+        X = self._add_constant(X)
+        return X @ self.w
--- linear_regression_tb.py
+++ linear_regression_tb.py
@@ -1,6 +1,94 @@
-from sklearn.base import BaseEstimator
-from sklearn.cluster._kmeans import KMeans
+import numpy as np
+import matplotlib.pyplot as plt
+import sklearn.linear_model
 
-k = KMeans()
+from alekseiml.regressions import LinearRegressionLMS, LinearRegressorGD, RandomNormalEstimator, RandomUniformEstimator, LinearRegressorGD2
+import pandas as pd
+from sklearn import metrics as skmt
+from sklearn import datasets as dts
 
-k.predict()
+
+def test_random_normal_estimator(X, y):
+    random_normal = RandomNormalEstimator()
+    random_normal.learn(X, y)
+    pred_norm = random_normal.infer(X)
+    return pred_norm
+
+
+def test_random_uniform_estimator(X, y):
+    random_uniform = RandomUniformEstimator()
+    random_uniform.learn(X, y)
+    pred_uniform = random_uniform.infer(X)
+    return pred_uniform
+
+
+def test_linear_regression_lms(X, y):
+    lms = LinearRegressionLMS()
+    lms.learn(X, y)
+    pred_lms = lms.infer(X)
+    return pred_lms
+
+
+def test_linear_regression_gd(X, y):
+    gd = LinearRegressorGD()
+    gd.learn(X, y)
+    pred_gd = gd.infer(X)
+    return pred_gd
+
+
+def test_regressions(X, y):
+    return {
+        'linear': test_linear_regression_lms(X, y),
+        'gd': test_linear_regression_gd(X, y),
+        'uniform': test_random_uniform_estimator(X, y),
+        'normal': test_random_normal_estimator(X, y)
+    }
+
+
+def plot_regressions(X, y):
+    reg_results = test_regressions(X, y)
+    plt.plot(X, y, 'o')
+    print('linear regression red')
+    print('linear regression with gd green')
+    print('uniform estimator blue')
+    print('normal estimator orange')
+    plt.plot(X, reg_results['linear'], '-r')
+    plt.plot(X, reg_results['gd'], '-g')
+    plt.plot(X, reg_results['uniform'], '-b')
+    plt.plot(X, reg_results['normal'], '-p')
+    plt.show()
+
+
+def predict_age_by_credit_car_dept(path):
+    data = pd.read_csv(path)
+    print(data.columns)
+    X = np.array(data[['Tenure_in_org_in_months', 'Age']].values, dtype=np.float64)
+    y = np.array(np.ravel(data[['GROSS']].values), dtype=np.float64)
+    X /= np.linalg.norm(X)
+    y /= np.linalg.norm(y)
+    plt.plot(X, y, 'o')
+    gd = LinearRegressionLMS()
+    # gd = LinearRegressorGD()
+    sk_gd = sklearn.linear_model.LinearRegression()
+    # X, y, true_coefs = dts.make_regression(n_samples=100, n_features=2, n_informative=3, random_state=0,
+    #                                                     coef=True, noise=10)
+    # X_train, X_test, y_train, y_test = sklearn.model_selection.train_test_split(X, y, random_state=0, train_size=0.7)
+
+    # gd.learn(X, y, n_epochs=1000000)
+    gd.learn(X, y)
+    sk_gd.fit(X, y)
+    pred_gd = gd.infer(X)
+
+    plt.plot(X, pred_gd, '-r')
+    plt.show()
+    print(skmt.r2_score(y, pred_gd))
+    print(skmt.r2_score(y, sk_gd.predict(X)))
+
+
+data = np.array([[5,30530, 50],[7,90000, 79],[15,159899, 124],[28,270564, 300]])
+X = data[:,[0]]
+y = data[:,[1]]
+print ("Independent variables:", X, type(X))
+print ("Dependent variable:", y, type(y))
+
+predict_age_by_credit_car_dept('./data/Employee_monthly_salary.csv')
